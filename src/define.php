a03cf2b0df00d2590f928f78dfb425eba09cc501
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-04-08 14:01:41 +0200

    Update define.php

--- src/define.php
+++ src/define.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Bermuda\Utils;
-define("Bermuda\Utils\server_schema_is_secure", (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+define("Bermuda\Utils\server_scheme_is_secure", (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
     || $_SERVER['SERVER_PORT'] == 443);
-const server_schema = server_schema_is_secure ? 'https' : 'http';
-define("Bermuda\Utils\server_name", server_schema . '://' . $_SERVER['SERVER_NAME']);
+const server_scheme = server_scheme_is_secure ? 'https' : 'http';
+define("Bermuda\Utils\server_name", server_scheme . '://' . $_SERVER['SERVER_NAME']);
