89bd63c67dfb5da96a1932d58afb424451531ced
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-12-14 23:40:04 +0200

    Delete define.php

--- src/define.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Bermuda\Utils;
-define("Bermuda\Utils\server_scheme_is_secure", (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-    || $_SERVER['SERVER_PORT'] == 443);
-const server_scheme = server_scheme_is_secure ? 'https' : 'http';
-define("Bermuda\Utils\server_name", server_scheme . '://' . $_SERVER['SERVER_NAME']);
