8914f4abb1206cf0278487ab2e81cefc10f93fb8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-02-08 19:44:54 +0200

    Update constants.php

--- src/constants.php
+++ src/constants.php
@@ -3,16 +3,16 @@
 namespace Bermuda\Utils;
 
 if (PHP_SAPI === 'cli') {
-    define('server_schema', '');
+    define('server_scheme', '');
     define('server_host', '');
-    define('server_schema_is_secure', '');
+    define('server_scheme_is_secure', '');
     define('server_port', '');
 } else {
-    define('server_schema', (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    define('server_scheme', (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
     || $_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http');
 
     define('server_host', $_SERVER['SERVER_NAME']);
-    define('server_schema_is_secure', server_schema == 'https');
+    define('server_scheme_is_secure', server_schema == 'https');
     define('server_port', $_SERVER['SERVER_PORT']);
 }
 
