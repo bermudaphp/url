bcfd5cd1c01f57b3a6bff5e93de050570200264d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-02-08 19:28:52 +0200

    Create UrlSegment.php

---
+++ src/UrlSegment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bermuda\Url;
+
+final class UrlSegment
+{
+    public const host = 'host';
+    public const scheme = 'scheme';
+    public const query = 'query';
+    public const user = 'user';
+    public const pass = 'pass';
+    public const port = 'port';
+    public const path = 'path';
+    public const fragment = 'fragment';
+}
